4f1f545324963a1e1a38cf9a6feafb16ddb5d200
Eloy Gomes <eloy.gomes@icloud.com>
2024-01-09 11:43:52 -0300

    0.2.0.0
    React router dom done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,50 @@
-function App() {
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import { Navbar } from "./components/navbar/Navbar";
+import { Home } from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import { Products } from "./pages/products/Products";
+import { Users } from "./pages/users/Users";
 
-  return (
-    <div>Hello World</div>
-  )
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/products", element: <Products /> },
+        { path: "/users", element: <Users /> },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
