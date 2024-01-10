884c100b6f0e305bed45d7e662551cd44abded93
Eloy Gomes <eloy.gomes@icloud.com>
2024-01-10 01:24:40 -0300

    0.5.0.0
    Deployment of top Deals component [done]

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,20 @@
+import TopBox from "../../components/topBox/TopBox";
 import "./home.scss";
 
 export const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="box box1">
+        <TopBox />
+      </div>
+      <div className="box box2">BOX2</div>
+      <div className="box box3">BOX3</div>
+      <div className="box box4">BOX4</div>
+      <div className="box box5">BOX5</div>
+      <div className="box box6">BOX6</div>
+      <div className="box box7">BOX7</div>
+      <div className="box box8">BOx8</div>
+      <div className="box box9">BOX9</div>
+    </div>
+  );
 };
