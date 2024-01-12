3375083bfdbd4a122f530e13d4d8f74047363c19
Eloy Gomes <eloy.gomes@icloud.com>
2024-01-12 06:47:42 -0300

    0.6.2.0
    .

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,9 +1,29 @@
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridColDef,
+  GridToolbar,
+  GridValueGetterParams,
+} from "@mui/x-data-grid";
 import "./datatable.scss";
+import { Grid } from "@mui/material";
 
 function DataTable() {
   const columns: GridColDef[] = [
     { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "avatar",
+      headerName: "Avatar",
+      width: 90,
+      renderCell: (params) => {
+        return <img src={params.row.img || "/noavatar.png"} alt="" />;
+      },
+    },
+    {
+      field: "name",
+      headerName: "Status",
+      width: 100,
+      type: "boolean",
+    },
     {
       field: "firstName",
       headerName: "First name",
@@ -20,7 +40,7 @@ function DataTable() {
       field: "age",
       headerName: "Age",
       type: "number",
-      width: 110,
+      width: 50,
       editable: true,
     },
     {
@@ -35,15 +55,51 @@ function DataTable() {
   ];
 
   const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 14, status: false },
+    {
+      id: 2,
+      lastName: "Lannister",
+      firstName: "Cersei",
+      age: 31,
+      status: true,
+    },
+    {
+      id: 3,
+      lastName: "Lannister",
+      firstName: "Jaime",
+      age: 31,
+      status: true,
+    },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 11, status: true },
+    {
+      id: 5,
+      lastName: "Targaryen",
+      firstName: "Daenerys",
+      age: null,
+      status: true,
+    },
+    {
+      id: 6,
+      lastName: "Melisandre",
+      firstName: null,
+      age: 150,
+      status: true,
+    },
+    {
+      id: 7,
+      lastName: "Clifford",
+      firstName: "Ferrara",
+      age: 44,
+      status: true,
+    },
+    {
+      id: 8,
+      lastName: "Frances",
+      firstName: "Rossini",
+      age: 36,
+      status: true,
+    },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65, status: "true" },
   ];
 
   return (
@@ -59,14 +115,19 @@ function DataTable() {
             },
           },
         }}
+        slots={{ toolbar: GridToolbar }}
         slotProps={{
           toolbar: {
             showQuickFilter: true,
+            quickFilterProps: { debounceMs: 500 },
           },
         }}
         pageSizeOptions={[10]}
         checkboxSelection
-        // disableRowSelectionOnClick
+        disableRowSelectionOnClick
+        disableColumnFilter
+        disableDensitySelector
+        disableColumnSelector
       />
     </div>
   );
