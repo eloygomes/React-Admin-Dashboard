757bef8580cb72a11fca5d70d57dbf48e2f6a011
Eloy Gomes <eloy.gomes@icloud.com>
2024-01-12 18:46:11 -0300

    0.7.1.0
    .

---
+++ src/components/add/Add.tsx
@@ -0,0 +1,29 @@
+import { GridColDef } from "@mui/x-data-grid";
+import "./add.scss";
+
+type Props = {
+  slug: string;
+  columns: GridColDef[];
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Add = (props: Props) => {
+  return (
+    <div className="add">
+      <div className="modal">
+        <span className="close">X</span>
+        <h1>Add new {props.slug}</h1>
+        <form action="">
+          {props.columns.map((column) => (
+            <div className="item">
+              <label>{column.headerName}</label>
+              <input type={column.type} placeholder={column.field} />
+            </div>
+          ))}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,67 +1,117 @@
 import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
-import DataTable from "../../components/dataTable/dataTable";
+import DataTable from "../../components/dataTable/DataTable";
 import "./users.scss";
 import { userRows } from "../../data";
+import { useState } from "react";
+import Add from "../../components/add/Add";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90 },
   {
-    field: "avatar",
+    field: "img",
     headerName: "Avatar",
-    width: 90,
+    width: 100,
     renderCell: (params) => {
       return <img src={params.row.img || "/noavatar.png"} alt="" />;
     },
   },
   {
-    field: "actions",
-    headerName: "Actions",
-    width: 90,
-    renderCell: (params) => {
-      return (
-        <div className="action">
-          <div className="view">View</div>
-          <div className="delete">Delete</div>
-        </div>
-      );
-    },
-  },
-  {
-    field: "name",
-    headerName: "Status",
-    width: 100,
-    type: "boolean",
-    editable: true,
-  },
-  {
     field: "firstName",
+    type: "string",
     headerName: "First name",
     width: 150,
-    editable: true,
   },
   {
     field: "lastName",
+    type: "string",
     headerName: "Last name",
     width: 150,
-    editable: true,
   },
   {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 50,
-    editable: true,
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 200,
   },
   {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
   },
 ];
+
+// const columns: GridColDef[] = [
+//   { field: "id", headerName: "ID", width: 90 },
+//   {
+//     field: "avatar",
+//     headerName: "Avatar",
+//     width: 90,
+//     renderCell: (params) => {
+//       return <img src={params.row.img || "/noavatar.png"} alt="" />;
+//     },
+//   },
+//   {
+//     field: "actions",
+//     headerName: "Actions",
+//     width: 90,
+//     renderCell: (params) => {
+//       return (
+//         <div className="action">
+//           <div className="view">View</div>
+//           <div className="delete">Delete</div>
+//         </div>
+//       );
+//     },
+//   },
+//   {
+//     field: "name",
+//     headerName: "Status",
+//     width: 100,
+//     type: "boolean",
+//     editable: true,
+//   },
+//   {
+//     field: "firstName",
+//     headerName: "First name",
+//     width: 150,
+//     editable: true,
+//   },
+//   {
+//     field: "lastName",
+//     headerName: "Last name",
+//     width: 150,
+//     editable: true,
+//   },
+//   {
+//     field: "age",
+//     headerName: "Age",
+//     type: "number",
+//     width: 50,
+//     editable: true,
+//   },
+//   {
+//     field: "fullName",
+//     headerName: "Full name",
+//     description: "This column has a value getter and is not sortable.",
+//     sortable: false,
+//     width: 160,
+//     valueGetter: (params: GridValueGetterParams) =>
+//       `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+//   },
+// ];
 
 const rows = [
   { id: 1, lastName: "Snow", firstName: "Jon", age: 14, status: false },
@@ -112,6 +162,7 @@ const rows = [
 ];
 
 export const Users = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="users">
       <div className="info">
@@ -119,6 +170,7 @@ export const Users = () => {
         <button>Add New User</button>
       </div>
       <DataTable slug="users" columns={columns} rows={userRows} />
+      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
     </div>
   );
 };
